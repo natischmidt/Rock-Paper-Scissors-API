dc45a037812e49f9c63a474094b3a4ff21146b6d
nati schmidt <nati.schmidt97@outlook.de>
2023-01-11 23:35:32 +0100

    ibland är man snabb... playerrepo ska vara ett interface!

--- src/main/java/com/example/api_rps/Player/PlayerRepo.java
+++ src/main/java/com/example/api_rps/Player/PlayerRepo.java
@@ -1,32 +1,27 @@
 package com.example.api_rps.Player;
-import com.example.api_rps.Player.PlayerEntity;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.FluentQuery;
-import org.springframework.stereotype.Component;
 
-import java.util.*;
-import java.util.function.Function;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Stream;
 
-@Component
 //getting error here wip
-public class PlayerRepo  extends JpaRepository<PlayerEntity, UUID> {
+public interface PlayerRepo extends JpaRepository<PlayerEntity, UUID> {
 
     Map<String, PlayerEntity> players= new HashMap<>();
 
-    public Stream<PlayerEntity> all() {
-        return players.values().stream();
-    }
-    public PlayerEntity save(PlayerEntity playerEntity) {
-        players.put(String.valueOf(playerEntity.getPlayerid()), playerEntity);
-        return playerEntity;
-    }
-    public Optional<PlayerEntity> get(UUID playerid) {
-        return Optional.ofNullable(players.get(playerid));
-    }
+//    public default Stream<PlayerEntity> all() {
+//        return players.values().stream();
+//    }
+//    public PlayerEntity save(PlayerEntity playerEntity) {
+//        players.put(String.valueOf(playerEntity.getPlayerid()), playerEntity);
+//        return playerEntity;
+//    }
+//    public Optional<PlayerEntity> get(UUID playerid) {
+//        return Optional.ofNullable(players.get(playerid));
+//    }
 
 }
