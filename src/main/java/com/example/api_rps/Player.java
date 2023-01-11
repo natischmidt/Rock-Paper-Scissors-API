89144d5a3ae767597b8916fa04ff9bd9e01a17aa
nati schmidt <nati.schmidt97@outlook.de>
2023-01-11 18:14:50 +0100

    Player klassen finns nu som ska kunna returnera namn och moves till andr klasser, en expection har laggts till sa att namnet inte kan vara tomt

---
+++ src/main/java/com/example/api_rps/Player.java
@@ -0,0 +1,26 @@
+package com.example.api_rps;
+
+
+public class Player {
+    private final String userName;
+    private Move playerMove;
+
+    //The username cant be empty or null so it throws an exception
+    Player(String name) throws IllegalArgumentException{
+        if (name != null && !name.isEmpty())
+            this.userName = name;
+        else {
+            throw new IllegalArgumentException("Username cant be empty");
+        }
+    }
+
+
+    String getPlayername(){
+        return userName;
+    }
+
+    Move getPlayerMove(){
+        return playerMove;
+    }
+
+}
