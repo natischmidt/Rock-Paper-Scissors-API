0316e76d658fbb0711c3584f24aba7f625c200a0
nati schmidt <nati.schmidt97@outlook.de>
2023-01-11 19:54:04 +0100

    la till en test fil WIP

--- src/main/java/com/example/api_rps/PlayerService.java
+++ src/main/java/com/example/api_rps/PlayerService.java
@@ -1,10 +1,18 @@
 package com.example.api_rps;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
 import java.util.UUID;
-
+import java.util.stream.Stream;
+@Service
+@AllArgsConstructor
 public class PlayerService {
-    PlayerRepo playerRepo;
 
+    PlayerRepo playerRepo;
+    public Stream<PlayerEntity> all() {
+        return playerRepo.all();
+    }
     public PlayerEntity createPlayer(UUID playerid) {
         PlayerEntity playerEntity = new PlayerEntity(
                 UUID.randomUUID()
--- src/main/java/com/example/api_rps/Token.java
+++ src/main/java/com/example/api_rps/Token.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,7 +16,19 @@ import java.util.stream.Collectors;
 //aka PlayerController
 public class Token {
 
+    PlayerService playerService;
+    private static List<Game> players = new ArrayList<>();
+
+
     @GetMapping
+    public List<Player> all() {
+        return playerService.all()
+                .map(Token::toDTO)
+                .collect(Collectors.toList());
+    }
+
+//    @GetMapping
+
     private static Player toDTO(PlayerEntity playerEntity) {
         return new Player(
         );
