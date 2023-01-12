96468c37a909c86a93e43650113d6ecb215a56a0
nati schmidt <nati.schmidt97@outlook.de>
2023-01-12 14:08:45 +0100

    make a move wip

--- src/main/java/com/example/api_rps/Games/GameController.java
+++ src/main/java/com/example/api_rps/Games/GameController.java
@@ -70,16 +70,6 @@ public GameContainer Info(@PathVariable("gameId") UUID gameId) throws GameNotFou
         String playerMove = body.get("move");
 
     }
-//    //Make move
-//    //wip
-//@PostMapping("/games/move/{sign}")
-//public GameContainer choosingSign(@PathVariable("sign") String sign,
-//                           @RequestHeader(value = "token") UUID playerid,
-//                           @RequestBody GameContainer gameContainer) throws GameNotFoundExeption {
-//    return gameService.choseSign(sign, playerid, gameContainer)
-//            .map(this::GametoDTO)
-//            .orElse(null);
-//}
 
 
 
