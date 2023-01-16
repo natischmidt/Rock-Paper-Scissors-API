6c59811304390babafe968609fa627a4009adefb
nati schmidt <nati.schmidt97@outlook.de>
2023-01-16 23:07:32 +0100

    ska göra om security delen wip

--- src/main/java/com/example/api_rps/Repository/RoleRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.api_rps.Repository;
-import com.example.api_rps.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepo extends JpaRepository <Role,Long>{
-    Role findByName(String name);
-
-}
--- src/main/java/com/example/api_rps/Repository/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.api_rps.Repository;
-
-import com.example.api_rps.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository <User,Long> {
-    User findByUsername(String username);
-}
