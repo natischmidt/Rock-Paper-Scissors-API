aa073d539397fe6e5fc1ecf0609ff24b27a9b402
nati schmidt <nati.schmidt97@outlook.de>
2023-01-16 23:23:55 +0100

    User klassen skapat, samt tabell

--- src/main/java/com/example/api_rps/User/User.java
+++ src/main/java/com/example/api_rps/User/User.java
@@ -1,4 +1,27 @@
 package com.example.api_rps.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+
+        @Id
+        @GeneratedValue
+        private Integer id;
+        private String username;
+        private String password;
+
+
 }
