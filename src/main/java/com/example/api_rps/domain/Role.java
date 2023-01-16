26cf2f2d62222d460eedefc50a2fbac857a80cf6
nati schmidt <nati.schmidt97@outlook.de>
2023-01-16 16:50:17 +0100

    nu finns de variablerna o anoontationer i user och role som beh;vs till en b;rjan

--- src/main/java/com/example/api_rps/domain/Role.java
+++ src/main/java/com/example/api_rps/domain/Role.java
@@ -1,4 +1,24 @@
 package com.example.api_rps.domain;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.GenerationType.AUTO;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Role {
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    private Long id;
+    private String name;
+
 }
