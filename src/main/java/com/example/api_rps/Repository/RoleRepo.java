b6ef37b7373bc7996a0ce7a4cb2a842084c92cdf
nati schmidt <nati.schmidt97@outlook.de>
2023-01-16 16:55:08 +0100

    Role och UserRepo skapat med tillh;rande methoder som returnerar rollen, alternativt namnet

---
+++ src/main/java/com/example/api_rps/Repository/RoleRepo.java
@@ -0,0 +1,10 @@
+package com.example.api_rps.Repository;
+
+
+import com.example.api_rps.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepo extends JpaRepository <Role,Long>{
+    Role findByName(String name);
+
+}
