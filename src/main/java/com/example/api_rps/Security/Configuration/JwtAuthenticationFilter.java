ccb780a3862b1273338b03264f678bc856348342
nati schmidt <nati.schmidt97@outlook.de>
2023-01-17 13:13:44 +0100

    jobbar vidare med authenticiaton

--- src/main/java/com/example/api_rps/Security/Configuration/JwtAuthenticationFilter.java
+++ src/main/java/com/example/api_rps/Security/Configuration/JwtAuthenticationFilter.java
@@ -12,6 +12,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
@@ -51,7 +52,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                         null,
                         userDetails.getAuthorities()
 
-                )
+                );
+                authenticationToken.setDetails(
+                        //Building the details out of the http request
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
             }
         }
 
