71f98e371fca7729f38b12827c8e99738a080b78
nati schmidt <nati.schmidt97@outlook.de>
2023-01-17 13:48:35 +0100

    security operator, s'tter upp hur filterna ska fungerar

--- src/main/java/com/example/api_rps/Security/Configuration/SecurityConfiguration.java
+++ src/main/java/com/example/api_rps/Security/Configuration/SecurityConfiguration.java
@@ -1,9 +1,11 @@
 package com.example.api_rps.Security.Configuration;
 
 
+import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -14,6 +16,9 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authenticationProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -34,7 +39,7 @@ public class SecurityConfiguration {
              .and()
              .authenticationProvider(authenticationProvider)
              //This filter before the usernamepassword filter
-             .addFilterBefore(JwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
      return httpSecurity.build();
     }
