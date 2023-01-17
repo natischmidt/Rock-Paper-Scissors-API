dc67cc692bb5815c098f4f203451162c0525ded3
nati schmidt <nati.schmidt97@outlook.de>
2023-01-17 13:05:55 +0100

    istället för att lägga till ene service klass, en configuration med Bean! wip

--- src/main/java/com/example/api_rps/Security/Configuration/ApplicationConfiguration.java
+++ src/main/java/com/example/api_rps/Security/Configuration/ApplicationConfiguration.java
@@ -1,5 +1,24 @@
 package com.example.api_rps.Security.Configuration;
 
+
+import com.example.api_rps.Repository.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
 public class ApplicationConfiguration {
 
+    private final UserRepo userRepo;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepo.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
 }
+
