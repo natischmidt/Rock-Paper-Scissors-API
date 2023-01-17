ef4687005564f9eda26651e7b86c92335e92626d
nati schmidt <nati.schmidt97@outlook.de>
2023-01-17 13:46:39 +0100

    security operator, s'tter upp hur filterna ska fungerar

--- src/main/java/com/example/api_rps/Security/Configuration/SecurityConfiguration.java
+++ src/main/java/com/example/api_rps/Security/Configuration/SecurityConfiguration.java
@@ -1,5 +1,44 @@
 package com.example.api_rps.Security.Configuration;
 
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+     httpSecurity
+             .csrf()
+             .disable()
+             .authorizeHttpRequests()
+             //These will be whitelisted
+             .requestMatchers("")
+             .permitAll()
+             //any other request will be authenticated
+             .anyRequest()
+             .authenticated()
+             .and()
+             .sessionManagement()
+             //Because im using once per request filter, the authentication state should not be stored
+             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+             .and()
+             .authenticationProvider(authenticationProvider)
+             //This filter before the usernamepassword filter
+             .addFilterBefore(JwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+     return httpSecurity.build();
+    }
+
+
+
 }
