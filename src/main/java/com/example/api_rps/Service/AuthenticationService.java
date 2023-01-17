2449e441b3edb4fafaf3137d884dfeef90f23834
nati schmidt <nati.schmidt97@outlook.de>
2023-01-17 14:17:23 +0100

    auth register wip

--- src/main/java/com/example/api_rps/Service/AuthenticationService.java
+++ src/main/java/com/example/api_rps/Service/AuthenticationService.java
@@ -1,10 +1,15 @@
 package com.example.api_rps.Service;
 
 
+import com.example.api_rps.Repository.GameRepo;
+import com.example.api_rps.Repository.UserRepo;
 import com.example.api_rps.Security.Authenticstion.AuthenticationRequest;
 import com.example.api_rps.Security.Authenticstion.AuthenticationResponse;
 import com.example.api_rps.Security.Authenticstion.RegisterRequest;
+import com.example.api_rps.User.Role;
+import com.example.api_rps.User.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,8 +17,24 @@ import org.springframework.stereotype.Service;
 
 public class AuthenticationService {
 
+    private final UserRepo userRepo;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
     public AuthenticationResponse register(RegisterRequest request) {
-        return null;
+        //Creating a user, save to the database and return the generated token
+        var user = User.builder()
+                .username(request.getUsername())
+                //Encode the password before saving it
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepo.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        //returning the generated token
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
