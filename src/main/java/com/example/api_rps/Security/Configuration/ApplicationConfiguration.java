2c7995b4e02724b750b65b8456b61bf157ae05c5
nati schmidt <nati.schmidt97@outlook.de>
2023-01-17 13:54:09 +0100

    authenticationprovider WIP

--- src/main/java/com/example/api_rps/Security/Configuration/ApplicationConfiguration.java
+++ src/main/java/com/example/api_rps/Security/Configuration/ApplicationConfiguration.java
@@ -5,6 +5,8 @@ import com.example.api_rps.Repository.UserRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
@@ -19,6 +21,16 @@ public class ApplicationConfiguration {
     public UserDetailsService userDetailsService() {
         return username -> userRepo.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User doesnt exist"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        //data acess object which is responsible to fetch the user details and encode passwords
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        //specifying which service is to be used
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 
 }
