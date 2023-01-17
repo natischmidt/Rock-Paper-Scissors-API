62a32d0db1a2cefae41c99250692233184730db4
nati schmidt <nati.schmidt97@outlook.de>
2023-01-17 12:10:17 +0100

    added jwt token dependency

--- src/main/java/com/example/api_rps/Security/Configuration/JwtAuthenticationFilter.java
+++ src/main/java/com/example/api_rps/Security/Configuration/JwtAuthenticationFilter.java
@@ -1,6 +1,7 @@
 package com.example.api_rps.Security.Configuration;
 
 
+import com.example.api_rps.Service.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,7 +15,7 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-
+    private final JwtService jwtService;
 
     @Override
     protected void doFilterInternal(
@@ -23,6 +24,17 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
            @NonNull  FilterChain filterChain)
             throws ServletException, IOException {
 
+        final String authenticationHeader = request.getHeader("Authorization");
+        final String JwtToken;
+        final String userName;
+        //Adding a check
+        if (authenticationHeader!= null && authenticationHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request,response);
+            return;
+        }
+        //Index 7 because of "bearer"
+        JwtToken = authenticationHeader.substring(7);
+        userName = jwtService.extractUserName(JwtToken);
 
     }
 }
--- src/main/java/com/example/api_rps/Service/JwtService.java
+++ src/main/java/com/example/api_rps/Service/JwtService.java
@@ -1,4 +1,12 @@
 package com.example.api_rps.Service;
 
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class JwtService {
+
+    public String extractUserName(String token) {
+        return null;
+    }
 }
