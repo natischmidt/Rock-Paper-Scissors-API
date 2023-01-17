f86fbfbea8e8859f7c2c56a62c04c565b2367abf
nati schmidt <nati.schmidt97@outlook.de>
2023-01-17 12:24:36 +0100

    workign with secret key and decoding wip

--- src/main/java/com/example/api_rps/Security/Authenticstion/AuthenticationResponse.java
+++ src/main/java/com/example/api_rps/Security/Authenticstion/AuthenticationResponse.java
@@ -1,4 +1,5 @@
 package com.example.api_rps.Security.Authenticstion;
 
 public class AuthenticationResponse {
+
 }
--- src/main/java/com/example/api_rps/Security/Configuration/ApplicationConfiguration.java
+++ src/main/java/com/example/api_rps/Security/Configuration/ApplicationConfiguration.java
@@ -1,4 +1,5 @@
 package com.example.api_rps.Security.Configuration;
 
 public class ApplicationConfiguration {
+
 }
